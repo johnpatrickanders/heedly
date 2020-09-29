e63bada7baf89327cfba0f8d3b7164c95553d4d0
johnpatrickanders <johnpatrickanders@gmail.com>
2020-09-29 10:56:16 -0400

    Basic Marial UI integration

--- client/src/App.js
+++ client/src/App.js
@@ -2,29 +2,38 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 
 import UserList from './components/UsersList';
-
+import { Header, Footer } from './components/layouts/index';
+import MyGrid from './components/articles/index';
+import Login from './components/layouts/login';
 
 function App() {
 
-  return (
-    <BrowserRouter>
-        <nav>
-            <ul>
-                <li><NavLink to="/" activeClass="active">Home</NavLink></li>
-                <li><NavLink to="/users" activeClass="active">Users</NavLink></li>
-            </ul>
-        </nav>
-        <Switch>
-            <Route path="/users">
-                <UserList />
-            </Route>
+    return (
+        <BrowserRouter>
+            <Header></Header>
+            <nav>
+                <ul>
+                    <li><NavLink to="/" activeClass="active">Home</NavLink></li>
+                    <li><NavLink to="/users" activeClass="active">Users</NavLink></li>
+                    <li><NavLink to="/login" activeClass="active">Login</NavLink></li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route path="/users">
+                    <UserList />
+                </Route>
+                <Route path="/login">
+                    <Login />
+                </Route>
 
-            <Route path="/">
-                <h1>My Home Page</h1>
-            </Route>
-        </Switch>
-    </BrowserRouter>
-  );
+                <Route path="/">
+                    <h1>My Home Page</h1>
+                </Route>
+            </Switch>
+            <MyGrid></MyGrid>
+            <Footer></Footer>
+        </BrowserRouter>
+    );
 }
 
 export default App;

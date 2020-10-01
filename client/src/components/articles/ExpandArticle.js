d45aa6c79eb7991bbb53bd2adb92b8dd74ad8e74
johnpatrickanders <johnpatrickanders@gmail.com>
2020-10-01 18:28:59 -0400

    clicking plus icon dispatches trivial data to db

--- client/src/components/articles/ExpandArticle.js
+++ client/src/components/articles/ExpandArticle.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Grid, Paper, Typography } from '@material-ui/core';
 import { useStyles } from '../layouts/Header';
 import Icon from '@material-ui/core/Icon';
-
+import { dispatchArticleMark } from '../../store/marks';
 
 const style = {
   Paper: { padding: 20, marginTop: 10, marginBottom: 10, overflowWrap: "break-word" },
@@ -14,13 +14,19 @@ const style = {
   }
 }
 
+// const PlusIcon = props => {
+
+//   return
+// }
+
 export default props => {
   const classes = useStyles();
+  const dispatch = useDispatch()
   let pageLoad = useSelector(state => state.news.pageContent);
   if (!pageLoad) {
     return null;
   }
-
+  const markAsRead = () => dispatch(dispatchArticleMark())
   console.log('content:', pageLoad.article)
   const article = pageLoad.article;
 
@@ -30,7 +36,7 @@ export default props => {
       <Paper style={style.Paper}>
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         <img src={article.img} style={style.img}></img>
-        <Icon style={{ fontSize: 30, marginTop: 15, display: 'block' }}>add_circle</Icon>
+        <Icon onClick={markAsRead} style={{ fontSize: 30, marginTop: 15, display: 'block' }}>add_circle</Icon>
         <h3>
           {article.title}
         </h3>
